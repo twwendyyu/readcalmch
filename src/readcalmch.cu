3580f95dab09b294d8f9077114b31b84b15feca1
Ting Wen Yu <wendyyu0123@gmail.com>
2017-03-28 23:29:59 +0800

    new 2D array

--- src/readcalmch.cu
+++ src/readcalmch.cu
@@ -16,17 +16,41 @@
 static void CheckCudaErrorAux (const char *, unsigned, const char *, cudaError_t);
 #define CUDA_CHECK_RETURN(value) CheckCudaErrorAux(__FILE__,__LINE__, #value, value)
 
-void initialize(float *data, unsigned size)
+void initialize1DArray(float *data, unsigned size)
 {
 	for (unsigned i = 0; i < size; ++i)
 		data[i] = 0.0;
 }
 
-void print1DArray(float *data, unsigned size)
+void initialize2DArray(float **data2D, unsigned *size_2D)
 {
-	FILE *fptr = fopen("printArray.txt","w");
+	unsigned height = size_2D[0];
+	unsigned width = size_2D[1];
+
+	for (unsigned i = 0; i < height; ++i){
+		for (unsigned j = 0; j < width; ++j)
+			data2D[i][j] = 0.0;
+	}
+}
+
+void fprintf1DArray(float *data, unsigned size)
+{
+	FILE *fptr = fopen("1DArray.txt","w");
 	for (unsigned i = 0; i < size; ++i)
 		fprintf(fptr,"[%d]\t%f\n",i,data[i]);
+	fclose(fptr);
+}
+
+void fprintf2DArray(float **data2D, unsigned *size_2D)
+{
+	unsigned height = size_2D[0];
+	unsigned width = size_2D[1];
+
+	FILE *fptr = fopen("2DArray.txt","w");
+	for (unsigned i = 0; i < height; ++i){
+		for (unsigned j = 0; j < width; ++j)
+			fprintf(fptr,"[%d][%d]\t%f\n",i,j,data2D[i][j]);
+	}
 	fclose(fptr);
 }
 
@@ -59,24 +83,68 @@ float *gpuReciprocal(float *data, unsigned size)
 	return rc;
 }
 
+/**
+ * Host function that copies the data and launches the work on GPU
+ */
+float *gpuCalReflectance(float *data, unsigned *size_2D)
+{
+	unsigned size = size_2D[0];
+	float *rc = new float[size];
+	float *gpuData;
+
+	CUDA_CHECK_RETURN(cudaMalloc((void **)&gpuData, sizeof(float)*size));
+	CUDA_CHECK_RETURN(cudaMemcpy(gpuData, data, sizeof(float)*size, cudaMemcpyHostToDevice));
+
+	static const int BLOCK_SIZE = 256;
+	const int blockCount = (size+BLOCK_SIZE-1)/BLOCK_SIZE;
+	reciprocalKernel<<<blockCount, BLOCK_SIZE>>> (gpuData, size);
+
+	CUDA_CHECK_RETURN(cudaMemcpy(rc, gpuData, sizeof(float)*size, cudaMemcpyDeviceToHost));
+	CUDA_CHECK_RETURN(cudaFree(gpuData));
+	return rc;
+}
+
 int main(void)
 {
-	static const int WORK_SIZE = 65;
+	/* 1D */
+	int WORK_SIZE = 100;
 	float *data = new float[WORK_SIZE];
-
-	initialize (data, WORK_SIZE);
+	initialize1DArray (data, WORK_SIZE);
 
 	float *recGpu = gpuReciprocal(data, WORK_SIZE);
-	print1DArray(recGpu, WORK_SIZE);
+	fprintf1DArray (recGpu, WORK_SIZE);
 
+	/* 2D */
+	unsigned *WORK_SIZE_2D = new unsigned[2];
+	WORK_SIZE_2D[0] = 100; /* array height */
+	WORK_SIZE_2D[1] = 2;   /* array width */
+
+	float **data2D = new float*[WORK_SIZE_2D[0]];
+	for(int i = 0; i < WORK_SIZE_2D[0]; ++i)
+		data2D[i] = new float[WORK_SIZE_2D[1]];
+	initialize2DArray (data2D, WORK_SIZE_2D);
+	fprintf2DArray (data2D, WORK_SIZE_2D);
+
+	float *refPerDet = gpuCalReflectance(data, WORK_SIZE_2D);
+
+
+
+	/* Sum up in host */
 	float gpuSum = std::accumulate (recGpu, recGpu+WORK_SIZE, 0.0);
+	float result = std::accumulate (refPerDet, refPerDet+WORK_SIZE, 0.0);
 
 	/* Verify the results */
 	std::cout<<"gpuSum = "<<gpuSum<<std::endl;
+	std::cout<<"result = "<<result<<std::endl;
 
 	/* Free memory */
 	delete[] data;
 	delete[] recGpu;
+	for(int i = 0; i < WORK_SIZE_2D[0]; ++i)
+		delete[] data2D[i];
+	delete[] data2D;
+	delete[] WORK_SIZE_2D;
+	delete[] refPerDet;
 
 	return 0;
 }
