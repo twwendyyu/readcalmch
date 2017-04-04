331b5d57131f745145a8e35f091f94b1fadc6ce7
Ting Wen Yu <wendyyu0123@gmail.com>
2017-04-04 12:47:04 +0800

    Conducted initloadpara()

--- src/readcalmch.cu
+++ src/readcalmch.cu
@@ -12,9 +12,30 @@
 #include <numeric>
 #include <stdlib.h>
 #include <stdio.h>
+#include <thrust/execution_policy.h>
+#include <thrust/sort.h>
+#include <thrust/reduce.h>
 
 static void CheckCudaErrorAux (const char *, unsigned, const char *, cudaError_t);
 #define CUDA_CHECK_RETURN(value) CheckCudaErrorAux(__FILE__,__LINE__, #value, value)
+
+typedef struct _MCHInfo {
+	char	fname_mch[30];
+	char	fname_inp[30];
+	char	magicheader[4];
+	unsigned int	version, maxmedia, detnum, colcount, totalphoton, detected, savedphoton, seedbyte;
+	unsigned int 	junk[5];
+	float 	unitmm, normalizer;
+	float 	na, n0, theta; 	// load from .inp
+	float 	*mua;			// load from .inp
+}MCHInfo;
+
+typedef struct _MCHData {
+	float *rawData;
+	unsigned int *detid;
+	float *weight;
+	float *result;
+}MCHData;
 
 void initialize1DArray(float *data, unsigned size)
 {
@@ -121,7 +142,7 @@ float *gpuReciprocal(float *data, unsigned size)
 /**
  * Host function that copies, reshape the data and launches the work on GPU
  */
-float *gpuRefPerDet(float **data2D, unsigned *size_2D)
+float *gpuRefPerPhoton(float **data2D, unsigned *size_2D)
 {
 	unsigned height = size_2D[0];
 	unsigned width  = size_2D[1];
@@ -147,59 +168,95 @@ float *gpuRefPerDet(float **data2D, unsigned *size_2D)
 	CUDA_CHECK_RETURN(cudaMemcpy(rc, gpuResult, sizeof(float)*sizeOfgpuResult, cudaMemcpyDeviceToHost));
 
 	CUDA_CHECK_RETURN(cudaFree(gpuData));
-
-	/* gpuRefPerDet */
-
-
-	//blockCount = (sizeOfgpuResult+BLOCK_SIZE-1)/BLOCK_SIZE;
-	//ifReductionKernel <<< blockCount, BLOCK_SIZE>>> ();
-
-	//CUDA_CHECK_RETURN(cudaFree(gpuResult));
+	CUDA_CHECK_RETURN(cudaFree(gpuResult));
 
 	return rc;
+}
+void initloadpara(MCHInfo *info, MCHData *data){
+
+	FILE *fptr_mch, *fptr_inp;
+
+	// set constant
+	info->n0 = 1.457; 	printf("n0\t\t%f\n",info->n0);
+	info->na = 0.22; 	printf("na\t\t%f\n",info->na);
+	info->theta = asin(info->na/info->n0); printf("theta\t\t%f\n",info->theta);
+
+	// specify .mch fname
+	printf("Enter .mch file name:");
+	scanf("%s",&(info->fname_mch));
+	printf("Loading from %s ...\n",info->fname_mch);
+
+	// load from fptr_mch
+	fptr_mch = fopen(info->fname_mch,"rb");
+
+	fread(info->magicheader,sizeof(char),4,fptr_mch);				printf("version\t\t%c%c%c%c\n",info->magicheader[0],info->magicheader[1],info->magicheader[2],info->magicheader[3]);
+	fread(&(info->version),sizeof(unsigned int),1,fptr_mch);		printf("version\t\t%d\n",info->version);
+	fread(&(info->maxmedia),sizeof(unsigned int),1,fptr_mch);		printf("mexmedia\t%d\n",info->maxmedia);
+	fread(&(info->detnum),sizeof(unsigned int),1,fptr_mch);			printf("detnum\t\t%d\n",info->detnum);
+	fread(&(info->colcount),sizeof(unsigned int),1,fptr_mch);		printf("colcount\t%d\n",info->colcount);
+	fread(&(info->totalphoton),sizeof(unsigned int),1,fptr_mch);	printf("totalphoton\t%d\n",info->totalphoton);
+	fread(&(info->detected),sizeof(unsigned int),1,fptr_mch);		printf("detected\t%d\n",info->detected);
+	fread(&(info->savedphoton),sizeof(unsigned int),1,fptr_mch);	printf("savedphoton\t%d\n",info->savedphoton);
+	fread(&(info->unitmm),sizeof(float),1,fptr_mch);				printf("unitmm\t\t%f\n",info->unitmm);
+	fread(&(info->seedbyte),sizeof(unsigned int),1,fptr_mch);		printf("seedbyte\t%d\n",info->seedbyte);
+	fread(&(info->normalizer),sizeof(float),1,fptr_mch);			printf("normalizer\t%f\n",info->normalizer);
+	fread(info->junk,sizeof(unsigned int),5,fptr_mch);				printf("junk\t\t%d%d%d%d%d\n",info->junk[0],info->junk[1],info->junk[2],info->junk[3],info->junk[4]);
+
+	//allocate memory
+	unsigned int sizeOfData = info->savedphoton;
+	data->detid = (unsigned int*) malloc (sizeof(unsigned int)*sizeOfData);
+	data->weight = (float*) malloc (sizeof(float)*sizeOfData);
+
+	unsigned int sizeOfResult = info->detnum;
+	data->result = (float*) malloc (sizeof(float)*sizeOfResult);
+
+	unsigned int sizeOfRawData = info->savedphoton*info->colcount;
+	data->rawData = (float*) malloc (sizeof(float)*sizeOfRawData);
+	fread(data->rawData ,sizeof(float), sizeOfRawData,fptr_mch); /* did not scaled back to 1 mm yet */
+
+
+	// specify .inp fname
+	printf("Enter .inp file name:");
+	scanf("%s",&(info->fname_inp));
+	printf("Loading from %s ...\n",info->fname_inp);
+
+	// load from fptr_inp
+	fptr_inp = fopen(info->fname_inp,"r");
+	char junkc[50];
+	for (int i = 0; i < 10; ++i){
+		fgets(junkc, 50, fptr_inp); //discard from line 1 to 10
+		printf("%s\n",junkc);
+	}
+	unsigned int sizeOfMua = info->maxmedia;
+	double junkf1, junkf2, junkf3, junkf4;
+	info->mua = (float*) malloc (sizeof(float)*sizeOfMua);
+	for(int i = 0; i < sizeOfMua; ++i)
+	{
+		printf("line %d:",i);
+		fscanf(fptr_inp,"%lf %lf %lf %lf",&(junkf1), &(junkf2), &(junkf3), &(junkf4));
+		info->mua[i] = (float)junkf3; //casting double into float, and stored in mua[i]
+		printf("\t%e\n",info->mua[i]);
+	}
+
+	// close
+	fclose(fptr_mch);
+	fclose(fptr_inp);
+
+}
+void calref_photon(MCHInfo *info,MCHData *data){
+
 }
 
 int main(void)
 {
-	/* 1D */
-	int WORK_SIZE = 128;
-	float *data = new float[WORK_SIZE];
-	initialize1DArray (data, WORK_SIZE);
+	MCHInfo info;
+	MCHData data;
 
-	float *recGpu = gpuReciprocal(data, WORK_SIZE);
-	fprintf1DArray (recGpu, WORK_SIZE);
-
-	/* 2D */
-	unsigned *WORK_SIZE_2D = new unsigned[2];
-	WORK_SIZE_2D[0] = 32; /* array height */
-	WORK_SIZE_2D[1] = 4;   /* array width */
-
-	float **data2D = new float*[WORK_SIZE_2D[0]];
-	for(int i = 0; i < WORK_SIZE_2D[0]; ++i)
-		data2D[i] = new float[WORK_SIZE_2D[1]];
-	initialize2DArray (data2D, WORK_SIZE_2D);
-	fprintf2DArray (data2D, WORK_SIZE_2D);
-
-	float *refPerDet = gpuRefPerDet(data2D, WORK_SIZE_2D);
-
-
-
-	/* Sum up in host */
-	float gpuSum = std::accumulate (recGpu, recGpu+WORK_SIZE, 0.0);
-	float result = std::accumulate (refPerDet, refPerDet+ WORK_SIZE_2D[0]*WORK_SIZE_2D[1], 0.0);
-
-	/* Verify the results */
-	std::cout<<"gpuSum = "<<gpuSum<<std::endl;
-	std::cout<<"result = "<<result<<std::endl;
-
-	/* Free memory */
-	delete[] data;
-	delete[] recGpu;
-	for(int i = 0; i < WORK_SIZE_2D[0]; ++i)
-		delete[] data2D[i];
-	delete[] data2D;
-	delete[] WORK_SIZE_2D;
-	delete[] refPerDet;
+	initloadpara(&info,&data);
+	calref_photon(&info,&data);	// void calref_photon(MCHInfo *info, MCHData *data);//also partition rawData into detid, weight arrays //__host__ calRefPerPhotonKernel
+	//sortbykey(&info,&data);		// void sortbykey(MCHInfo *info, MCHData *data); 	//__host__ void thrust::sort_by_key
+	//calref_det(&info,&data);		// void calref(MCHInfo *info, MCHData *data);		//__host__ thrust::pair<float*,float*> thrust::reduce_by_key
+	//printresult(&info,&data);		// void printresult(MCHInfo *info, MCHData *data);
 
 	return 0;
 }
